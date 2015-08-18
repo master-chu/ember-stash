09297914361dc6a38542c4bfe3a09e702a856b06
Christopher Chu <cchu@zipcar.com>
2015-08-18 11:06:38 -0400

    extracted messages into their own file

--- logger.js
+++ logger.js
@@ -1,20 +1,7 @@
-var colors  = require('colors'),
-    _       = require('lodash'),
-    sprintf = require("sprintf-js").sprintf;
-
-//TODO: make it its own file
-var templates = {
-  'prompt_for_credentials': 'Please enter your credentials for ' + 'stash.zipcar.com.'.yellow,
-  'log_in_success': 'Logged in as Stash user %s.',
-  'force_login_instructions': 'To login as a different user, do ./run.sh -l',
-  'fetch_failed': 'Fetching pull requests for %s failed',
-  'hi_wrong_page': 'Hi, %s! You probably misread the cli instructions. To access the app, just open index.html in your browser.',
-  'log_in_failure': 'Login for user %s failed: statusCode %s',
-  'reason_for_login_failure': 'Either there are network issues, or you typed your username or password incorrectly',
-  'server_started_on_port': 'Stash proxy started on http://localhost:%s',
-  'to_access_app': 'To access the app, just open ' + 'index.html'.yellow + ' in your browser.',
-  'key_does_not_exist': 'The message key you provided does not exist'
-};
+var colors           = require('colors'),
+    _                = require('lodash'),
+    sprintf          = require('sprintf-js').sprintf,
+    messageTemplates = require('./message_templates.js');
 
 module.exports.logger = {
   notice: function(key){
@@ -37,7 +24,7 @@ module.exports.logger = {
   
 module.exports.messageLookup = function(key){
   var templateArgs = buildTemplateArgs(arguments);
-  var template = templates[key];
+  var template = messageTemplates[key];
   templateArgs.unshift(template);
   return substituteArgsIntoTemplate(templateArgs);
 }
@@ -59,10 +46,10 @@ function log(key, color, templateArgs){
 };
 
 function getTemplate(key, color){
-  if (templates.hasOwnProperty(key)) {
-    return templates[key][color];
+  if (messageTemplates.hasOwnProperty(key)) {
+    return messageTemplates[key][color];
   } else {
-    return templates['key_does_not_exist']['magenta'];
+    return messageTemplates['key_does_not_exist']['magenta'];
   }
 };
 
---
+++ message_templates.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'prompt_for_credentials': 'Please enter your credentials for ' + 'stash.zipcar.com.'.yellow,
+  'log_in_success': 'Logged in as Stash user %s.',
+  'force_login_instructions': 'To login as a different user, do ./run.sh -l',
+  'fetch_failed': 'Fetching pull requests for %s failed',
+  'hi_wrong_page': 'Hi, %s! You probably misread the cli instructions. To access the app, just open index.html in your browser.',
+  'log_in_failure': 'Login for user %s failed: statusCode %s',
+  'reason_for_login_failure': 'Either there are network issues, or you typed your username or password incorrectly',
+  'server_started_on_port': 'Stash proxy started on http://localhost:%s',
+  'to_access_app': 'To access the app, just open ' + 'index.html'.yellow + ' in your browser.',
+  'key_does_not_exist': 'The message key you provided does not exist'
+};
